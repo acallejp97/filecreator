2475d982bc6db5c8d62b1df195664e920c1e574a
acallejp97 <acallejp@gmail.com>
2021-08-10 09:17:21 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,21 +3,29 @@
 
 import fileinput
 import os
+import shutil
+
+lines = []
+
+initialFiles = 67
+quantity = 4
+dirName = "filecreator"
 
 
-with open("fechas.txt") as file_in:
-    lines = []
-    for line in file_in:
-        line = line.replace("\n", "")
-        lines.append(line)
+if os.path.isdir("./%s" % dirName):
+    shutil.rmtree("./%s" % dirName)
 
-for linea in lines:
+os.mkdir("./%s" % dirName)
+
+shutil.copyfile('example.xml', 'example.xml.bak')
+
+
+for linea in range(1, (initialFiles*quantity)):
 
     with fileinput.FileInput("example.xml", inplace=True, backup='.bak') as file:
         for line in file:
-            print(line.replace("REPLACE_ME", linea), end='')
-    nombre= linea.replace(":", "=")
-    os.rename('example.xml', '%s.xml' % nombre)
-    os.rename('example.xml.bak', 'example.xml')
+            print(line.replace("REPLACE_ME", str(linea)), end='')
+    nombre = str(linea)
+    os.rename('example.xml', './%s/%s.xml' % (dirName, nombre))
+    shutil.copyfile('example.xml.bak', 'example.xml')
     print("%s creado" % nombre)
-    
